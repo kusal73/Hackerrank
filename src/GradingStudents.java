8b31e50ed1b349045f8d8c469247ccde5452f24d
Kusal <kusalmohanty73@gmail.com>
2020-07-27 00:43:34 +0530

    Grading Students Solution

--- src/GradingStudents.java
+++ src/GradingStudents.java
@@ -1,9 +1,35 @@
 /*https://www.hackerrank.com/challenges/grading/problem*/
 
+import java.util.Scanner;
 public class GradingStudents
 {
-    public static void main(String[] args)
+    static int nearest_five(int number)
     {
+        return number+5-(number%5);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int no_of_students;
+        int i;
+        no_of_students = sc.nextInt();
+        int[] marks = new int[no_of_students];
+        for (i = 0; i < no_of_students; i++)
+        {
+            marks[i]=sc.nextInt();
+        }
+
+        for(i=0;i<no_of_students;i++)
+        {
+            if(marks[i]>=38)
+            {
+                int next_of_five=nearest_five(marks[i]);
+                if(next_of_five - marks[i]<3)
+                {
+                    marks[i]=next_of_five;
+                }
+            }
+            System.out.println(marks[i]);
+        }
 
     }
 }
